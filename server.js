20fc1bcb883ca5ea81841a2fb319ed649e5203f5
freenokke <kosmatkov94@gmail.com>
2022-08-18 13:38:50 +0300

    change data

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
+
+const db = {
+  garage: [
+      {
+          "name": "Tesla",
+          "color": "#e6e6fa",
+          "id": 1,
+      },
+      {
+          "name": "BMW",
+          "color": "#fede00",
+          "id": 2,
+      },
+      {
+          "name": "Mersedes",
+          "color": "#6c779f",
+          "id": 3,
+      },
+      {
+          "name": "Ford",
+          "color": "#ef3c40",
+          "id": 4,
+      },
+  ],
+  winners: [
+      {
+          id: 1,
+          wins: 1,
+          time: 10,
+      }
+  ]
+};
+
+const router = jsonServer.router(db);
 
 server.use(middlewares);
 server.use(router);
